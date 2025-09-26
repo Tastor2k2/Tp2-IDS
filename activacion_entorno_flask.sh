ae20efd1428b4c2af21b6477aa4cf569343d1589
Tastor2k2 <166441527+Tastor2k2@users.noreply.github.com>
2025-09-25 21:29:10 -0300

    Script acrivacion limpiado, insertado todo en funciones

--- activacion_entorno_flask.sh
+++ activacion_entorno_flask.sh
@@ -13,34 +13,34 @@ PROJECT_FOLDER_NAME=$1
 
 cd $PROJECT_FOLDER_NAME
 
-verificarInstalacionPython3() {
-    if python3 --version ; then
+instalarPython3() {
+    if python3 --version > /dev/null 2>&1 ; then
     echo ""
     echo "---------------------------Python3 ya esta instalado---------------------------"
     echo ""
     else
         echo ""
-        echo "---------------------------Python3 se instalara---------------------------"
+        echo "---------------------------Python3 se instalará---------------------------"
         echo ""
         sudo apt install python3
     fi
 }
 
-verificarInstalacionPip3() {
-    if pip3 --version ; then
+instalarPip3() {
+    if pip3 --version > /dev/null 2>&1 ; then
     echo ""
     echo "---------------------------Pip3 ya esta instalado---------------------------"
     echo ""
     else
         echo ""
-        echo "---------------------------Pip3 se instalara---------------------------"
+        echo "---------------------------Pip3 se instalará---------------------------"
         echo ""
         sudo apt install python3-pip
     fi
 }
 
 instalarFlaskMail() {
-    if pip list | grep Flask-Mail ; then
+    if pip list | grep Flask-Mail > /dev/null 2>&1 ; then
     echo ""
     echo "---------------------------Flask-Mail ya está instalado---------------------------"
     echo ""
@@ -52,24 +52,58 @@ instalarFlaskMail() {
     fi
 }
 
+crearVenv() {
+    if [[ -d ".venv" ]]; then
+        echo ""
+        echo "---------------------------Carpeta .venv ya estaba creada---------------------------"
+        echo ""
+    else
+        echo ""
+        echo "---------------------------Creando .venv---------------------------"
+        echo ""
+        python3 -m venv .venv # Comando para crear la carpeta .venv para el entorno.
+    fi
+}
+
+ActivacionEntornoVirtual() {
+    if [[ -z "$VIRTUAL_ENV" ]]; then
+        echo ""
+        echo "---------------------------Se activará el entorno virtual---------------------------"
+        echo ""
+        source .venv/bin/activate # activacion del entorno virtual
+    else
+        echo ""
+        echo "----------------Entorno virtual activado previamente en: $VIRTUAL_ENV----------------"
+        echo ""
+    fi
+}
+
 crearEnv() {
-    echo "---------------------------Creando .env---------------------------"
-    echo ""
-    touch .env
-    echo "---------------------------Llenando .env---------------------------"
-    echo ""
-    echo "SECRET_KEY=una-clave-secreta-muy-larga-y-unica" >> .env
-    echo "MAIL_SERVER=smtp.gmail.com" >> .env
-    echo "MAIL_PORT=587" >> .env
-    echo "MAIL_USE_TLS=True" >> .env
-    echo "MAIL_USE_SSL=False" >> .env
-    echo "MAIL_USERNAME=tu-email@gmail.com" >> .env
-    echo "MAIL_PASSWORD=tu-app-password" >> .env
-    echo "MAIL_DEFAULT_SENDER=tu-email@gmail.com" >> .env
+    if [[ -f ".env" ]]; then
+        echo ""
+        echo "---------------------------Archivo .env ya estaba creado---------------------------"
+        echo ""
+    else
+        echo ""
+        echo "---------------------------Creando .env---------------------------"
+        echo ""
+        touch .env
+        echo ""
+        echo "---------------------------Llenando .env---------------------------"
+        echo ""
+        echo "SECRET_KEY=una-clave-secreta-muy-larga-y-unica" >> .env
+        echo "MAIL_SERVER=smtp.gmail.com" >> .env
+        echo "MAIL_PORT=587" >> .env
+        echo "MAIL_USE_TLS=True" >> .env
+        echo "MAIL_USE_SSL=False" >> .env
+        echo "MAIL_USERNAME=tu-email@gmail.com" >> .env
+        echo "MAIL_PASSWORD=tu-app-password" >> .env
+        echo "MAIL_DEFAULT_SENDER=tu-email@gmail.com" >> .env
+    fi
 }
 
 instalarDotenv() {
-    if pip show python-dotenv ; then
+    if pip list | grep python-dotenv > /dev/null 2>&1 ; then
         echo ""
         echo "---------------------------python-dotenv ya está instalado---------------------------"
         echo ""
@@ -81,24 +115,35 @@ instalarDotenv() {
     fi
 }
 
-verificarInstalacionPython3
+instalarFlask() {
+    if pip list | grep Flask > /dev/null 2>&1 ; then
+        echo ""
+        echo "---------------------------Flask ya estaba instalado---------------------------"
+        echo ""
+    else
+        echo ""
+        echo "---------------------------Instalando Flask---------------------------"
+        echo ""
+        pip install flask
+    fi
+}
 
-verificarInstalacionPip3
+instalarPython3
+
+instalarPip3Pip3
 
 sudo apt install python3.12-venv
 
-echo ""
-echo "---------------------------Creando .venv---------------------------"
-echo ""
-python3 -m venv .venv # Comando para crear la carpeta .venv para el entorno.
-crearEnv # crea el archivo .env para los datos del mail emisor
-echo "---------------------------Activando el entorno---------------------------"
-echo ""
-source .venv/bin/activate # activacion del entorno virtual
-echo "---------------------------Instalando Flask---------------------------"
-echo ""
-pip install flask
+crearVenv
+
+crearEnv
+
+ActivacionEntornoVirtual
+
+instalarFlask
+
 instalarFlaskMail
+
 instalarDotenv
 
 
